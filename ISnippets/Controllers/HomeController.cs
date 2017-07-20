74d7668e722f6e0517ca52b186c8ff156b46ad59
Luis Paredes <lparedesl@gmail.com>
2017-07-20 10:40:50 -0400

    modified home controller to show snippets

--- ISnippets/Controllers/HomeController.cs
+++ ISnippets/Controllers/HomeController.cs
@@ -2,34 +2,140 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ISnippets.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ISnippets.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly MyDbContext _snippetsDb;
+
+        public HomeController(MyDbContext snippetsDb)
+        {
+            _snippetsDb = snippetsDb;
+        }
+
+        // GET: /
+        public async Task<IActionResult> Index()
+        {
+            return View(await _snippetsDb.Snippets.ToListAsync());
+        }
+
+        // GET: /Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var snippet = await _snippetsDb.Snippets.SingleOrDefaultAsync(m => m.Id == id);
+            if (snippet == null)
+            {
+                return NotFound();
+            }
+
+            return View(snippet);
+        }
+
+        // GET: /Create
+        public IActionResult Create()
         {
             return View();
         }
 
-        public IActionResult About()
+        // POST: /Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,CodeSnippet,Language")] Snippet snippet)
         {
-            ViewData["Message"] = "Your application description page.";
+            if (!ModelState.IsValid) return View(snippet);
 
-            return View();
+            _snippetsDb.Snippets.Add(snippet);
+            await _snippetsDb.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
-        public IActionResult Contact()
+        // GET: /Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
-            ViewData["Message"] = "Your contact page.";
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-            return View();
+            var snippet = await _snippetsDb.Snippets.SingleOrDefaultAsync(m => m.Id == id);
+            if (snippet == null)
+            {
+                return NotFound();
+            }
+            return View(snippet);
         }
 
-        public IActionResult Error()
+        // POST: /Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,CodeSnippet,Language")] Snippet snippet)
         {
-            return View();
+            if (id != snippet.Id)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid) return View(snippet);
+
+            try
+            {
+                _snippetsDb.Update(snippet);
+                await _snippetsDb.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SnippetExists(snippet.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: /Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var snippet = await _snippetsDb.Snippets.SingleOrDefaultAsync(m => m.Id == id);
+            if (snippet == null)
+            {
+                return NotFound();
+            }
+
+            return View(snippet);
+        }
+
+        // POST: /Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var snippet = await _snippetsDb.Snippets.SingleOrDefaultAsync(m => m.Id == id);
+            _snippetsDb.Snippets.Remove(snippet);
+            await _snippetsDb.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool SnippetExists(int id)
+        {
+            return _snippetsDb.Snippets.Any(e => e.Id == id);
         }
     }
 }
